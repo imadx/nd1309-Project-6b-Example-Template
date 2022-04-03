5c4b21fde24e26ac2be183329b49ad8eb37bcda9
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2022-04-03 11:57:12 +0530

    update rinkeby config

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,23 @@
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "3d3be56019e8461ab34d66fe3669b83f";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 9545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000
     }
   }
 };
